5261071e1f73f4f48bc5a815bf99711fd659a3a1
3nsar <84131267+3nsar@users.noreply.github.com>
2022-11-11 12:44:06 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,59 @@
-import InputBar from "./InputBar";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {db} from "./firebase"
+import { addDoc, collection, getDocs, doc, deleteDoc, onSnapshot } from "firebase/firestore";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import { AiOutlinePlus } from 'react-icons/ai'
+import Todo from './Todo';
+import { Container } from 'react-bootstrap';
 
 function App() {
+  
+  const [todo, setTodo] = useState([])
+  const [newTodo, setNewTodo] = useState("")
+  const todoCollectionRef = collection(db, "todos")
+
+  const createTodo = async () =>{
+    await addDoc(todoCollectionRef, {todo: newTodo})
+  }
+
+  const handleDelete = async (id) =>{
+    const todoDoc = doc(db, "todos", id);
+    await deleteDoc(todoDoc);
+  }
+
+
+  useEffect(() =>     
+     onSnapshot(collection(db, "todos"), (snapshot) =>
+        setTodo(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+      
+  ),[] );
+
   return (
-    <div>
-      <InputBar />
-    </div>
+    <Container >
+      <InputGroup className="mb-4 mt-4" >
+       <Form.Control
+      placeholder="Enter something..."
+      aria-label="Recipient's username"
+      aria-describedby="basic-addon2"
+      size= "lg"
+      
+      onChange={(e) =>{setNewTodo(e.target.value)}}
+    />
+    <Button variant="primary" id="button-addon2" onClick={createTodo}>
+    <AiOutlinePlus size={42}/>
+    </Button>
+  </InputGroup>
+      {todo.map((td, index)=>{
+        return(
+          <div>
+            <Todo key={index} td={td} handleDelete={handleDelete}/>
+          </div>
+        )
+      })}
+    </Container>
   );
 }
 
---
+++ src/Todo.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Card from 'react-bootstrap/Card';
+import { MdDelete } from 'react-icons/md'
+
+const Todo = ({td, handleDelete}) => {
+  return (
+    <div>
+    {/*<h1>{td.todo}</h1> */}
+
+    <Card  bg="light" body>
+       {td.todo}
+       <MdDelete className="btn-delete" size={28} onClick={() => {handleDelete(td.id)}}/>
+    </Card>
+    </div>
+  )
+}
+
+export default Todo
